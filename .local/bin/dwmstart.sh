d5a91992f59f6226e96cdaa5b001e9650db4c407
Ron Ivie <rwivie@gmail.com>
2023-09-23 11:06:14 -0500

    add .local/bin/dwmstart.sh

---
+++ .local/bin/dwmstart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+xrdb merge ~/.Xresources &
+nitrogen --restore &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+(sleep 5; nm-applet) &
+(sleep 5; blueman-applet) &
+numlockx on &
+mpDris2 &
+dunst -conf ~/.config/dunst/dunstrc &
+picom --config ~/.config/picom/picom_dwm.conf --daemon &
+(sleep 1; xautolock -time 10 -locker lock -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
+sleep 10 && mpd &
+
+sleep 15 && $HOME/.config/dwmblocks-async/build/dwmblocks &
+exec dbus-launch --sh-syntax --exit-with-session $HOME/.config/dwm-6.4/dwm 2>&1 | tee ~/.dwm.log
