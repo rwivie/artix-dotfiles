ed406c61c1773023bbe3aa1279139ce2a9057358
Ron Ivie <rwivie@gmail.com>
2023-10-11 12:07:37 -0500

    add .local/bin/dwmstart.sh

--- .local/bin/dwmstart.sh
+++ .local/bin/dwmstart.sh
@@ -7,6 +7,7 @@ nitrogen --restore &
 (sleep 5; blueman-applet) &
 numlockx on &
 mpDris2 &
+firewall-applet &
 dunst -conf ~/.config/dunst/dunstrc &
 picom --config ~/.config/picom/picom_dwm.conf --daemon &
 (sleep 1; xautolock -time 10 -locker lock -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
