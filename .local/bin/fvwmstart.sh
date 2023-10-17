dc0e351b8979f58107c48693b02e76a6150411ae
Ron Ivie <rwivie@gmail.com>
2023-10-16 23:40:49 -0500

    add .local/bin/fvwmstart.sh

--- .local/bin/fvwmstart.sh
+++ .local/bin/fvwmstart.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 xrdb merge ~/.Xresources &
+#playerctld daemon &
 nitrogen --restore &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 (sleep 5; nm-applet) &
 (sleep 5; blueman-applet) &
 numlockx on &
+#sleep 40 && mpDris2 &
 mpDris2 &
 firewall-applet &
 sleep 5 && pasystray --notify=none &
@@ -14,6 +16,7 @@ dunst -conf ~/.config/dunst/dunstrc &
 picom --config ~/.config/picom/picom.conf --daemon &
 (sleep 1; xautolock -time 10 -locker lock -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
 #sleep 10 && mpd &
-playerctl_fix.sh &
+#playerctl_fix.sh &
+#restart_mpdris2.sh &
 exec dbus-launch --sh-syntax --exit-with-session /usr/bin/fvwm3 2>&1 | tee ~/.fvwm.log
 
